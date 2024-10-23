bc0bc2dc94d2723016d2b57ba6431b3120b7c68e
Bejenar Nicolai <admynyral2808@gmail.com>
2024-10-23 08:50:00 +0300

    Lab #03 object

---
+++ lab03.cpp
@@ -0,0 +1,25 @@
+﻿#include <array>
+
+struct Leg
+{
+    int length;
+};
+
+struct Arm
+{
+    int power;
+};
+
+struct Person
+{
+    std::array<Leg, 2> legs;
+    Arm arms[2];
+};
+
+int main()
+{
+    Person person;
+}
+
+// Leg, int, Arm, Person, std::array<Leg, 2>
+// length, power, legs[0], legs[1], arms[0], arms[1], person
