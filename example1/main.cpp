aad6854f03e7867fa5cde81fef926bfcbb7b62c1
Bejenar Nicolai <admynyral2808@gmail.com>
2024-10-25 08:17:26 +0300

    Lab #06 linker

---
+++ example1/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int hello(int i);
+
+int main() {
+    cout << hello(1) << endl;
+}
+
+// Без этих строчек компилятор выдаст ошибку
+int hello(int i) {
+    return i + 1;
+}
+
---
+++ example2/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//int hello(int i);
+
+// В финальном исполняемом файле будет только одна копия функции hello
+static int hello(int i) {
+    return i + 4;
+}
+
+// 6. Если компилировать без f.cpp, то будет ошибка компиляции
+int main() {
+    cout << hello(1) << endl;
+}
